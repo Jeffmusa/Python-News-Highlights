522529a40422e749f1ecc6b096204f2ae25cde42
Jeff Musa <jeffmusa@gmail.com>
2018-09-01 15:34:59 +0300

    link to news page added

--- app/models/news.py
+++ app/models/news.py
@@ -5,3 +5,13 @@ class News:
         self.country = country
         self.id = id
         self.description = description
+
+
+class Articles:
+    def __init__(self,author,title,description,url,poster,time):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.poster = poster
+        self.time = time          
--- app/request.py
+++ app/request.py
@@ -54,3 +54,49 @@ def process_results(news_list):
         news_results.append(news_object)
         
     return news_results    
+
+
+
+
+def get_articles(id):
+    get_articles_url = articles_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
+
+        articles_results = None
+
+
+        if get_articles_response['articles']:
+            articles_results_list = get_articles_response['articles']
+            articles_results = process_results(articles_results_list)
+
+
+    return articles_results
+
+
+
+def process_articles(articles_list):
+    '''
+    Function  that processes the news result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain news details
+
+    Returns :
+        news_results: A list of news objects
+    '''
+    articles_results = []
+    for articles_item in articles_list:
+        author = articles_item.get('author')
+        title = articles_item.get('title')
+        description = articles_item.get('description')
+        url = articles_item.get('url')
+        poster = articles_item.get('urlToImage')
+        time = articles_item.get('publishedAt')
+        
+        articles_object = Articles(author,title,description,url,poster,time)
+        articles_results.append(articles_object)
+        
+    return articles_results    
